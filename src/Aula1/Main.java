ccd19fda856eb13398b275841ff9ce6391f9e320
fcampos <felipemoreiracp@hotmail.com>
2021-12-30 16:25:50 -0300

    first class

---
+++ src/Aula1/Main.java
@@ -0,0 +1,52 @@
+package Aula1;
+
+
+class Main {
+}
+
+    public static void main(String[] args) {
+
+        int i;
+        //int i;
+        int I;
+        //int 1a;
+        int _1a;
+        int $aq;
+
+        i = 5
+        I = 10
+        _1a = 20;
+        $aq = 7;
+
+        final int j = 10;
+        //j = 15;
+        int asrn24678md;
+        //int asrn246 78md;
+        int asrn2$4678_md = 10; // nao e uma boa pratica usar o $
+        //int asrn2$46%78_md = 10; nao usar é proibido usar %
+
+        int asrn24678md = 100;
+        int asrn2$4678_md = 10; // nao e uma boa pratica usar o $
+
+        int quantidadeProduto = 50;
+        int quantidadeProduto;
+        final int NUMERO_TENTATIVAS = 5;
+        final int numero_tentativas = 5;
+        int QUANTIDADE_OPCOES = 25;
+        int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asrn24678md);
+        System.out.println(asrn2$4678_md);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+
+    }
